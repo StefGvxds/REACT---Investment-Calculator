738bad2400cbc04dd3c8271297a25686b3caea5b
Stefanos Gavouchidis <stefanos.gavouchidis@gmail.com>
2024-05-28 14:39:45 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import React from "react";
+import "./index.css";
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <header id="header">
+        <img src="investment-calculator-logo.png" alt="Broken Image"></img>
+        <h1>React Investment Calculator</h1>
+      </header>
+      <body>
+        <div id="user-input">
+          <form className="input-group">
+            <label>Block 1</label>
+            <input type="text"></input>
+            <label>Block 2</label>
+            <input type="text"></input>
+            <label>Block 3</label>
+            <input type="text"></input>
+            <label>Block 4</label>
+            <input type="text"></input>
+          </form>
+        </div>
+      </body>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,7 @@
+export default function UserInput() {
+  return (
+    <>
+      <input type="text"></input>
+    </>
+  );
+}
